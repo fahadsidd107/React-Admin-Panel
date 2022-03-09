6d7d5ca1edfe30bc71fa6105d71b84b41984b18f
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-10 00:18:17 +0500

    work complete

---
+++ src/Data.js
@@ -0,0 +1,74 @@
+export const userAndSalesData = [
+    {
+      name: "Jan",
+      sales: 4000,
+      users: 2400,
+     
+    },
+    {
+      name: "Feb",
+      sales: 3000,
+      users: 1398,
+    
+    },
+    {
+      name: "March",
+      sales: 2000,
+      users: 9800,
+      
+    },
+    {
+      name: "April",
+      sales: 2780,
+      users: 3908,
+    
+    },
+    {
+      name: "May",
+      sales: 1890,
+      users: 4800,
+    
+    },
+    {
+      name: "June",
+      sales: 2390,
+      users: 3800,
+      
+    },
+    {
+      name: "July",
+      sales: 3490,
+      users: 4300,
+    
+    },
+    {
+      name: "Aug",
+      sales: 4000,
+      users: 2400,
+  
+    },
+    {
+      name: "Sept",
+      sales: 3000,
+      users: 1398,
+    
+    },
+    {
+      name: "Oct",
+      sales: 2000,
+      users: 9800,
+  
+    },
+    {
+      name: "Nov",
+      sales: 2780,
+      users: 3908,
+  
+    },
+    {
+      name: "Dec",
+      sales: 1890,
+      users: 4800,
+    },
+  
+  ];
