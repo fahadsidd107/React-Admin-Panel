ea10e9badde040f6892cd4ab0e199c8f71a6517c
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-10 00:17:49 +0500

    work complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './Home.css'
-
+import { userAndSalesData } from '../../Data'
 function Home() {
   return (
     <div className='home'>
     <FeaturedInfo/>
-    <Chart/>
+    <Chart title="Sales and Users Analytics" data={ userAndSalesData } userDataKey="users" salesDataKey='sales' grid/>
     </div>
   )
 }
