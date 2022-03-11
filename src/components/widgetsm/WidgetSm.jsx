3ec2fe77a1733cf53e66e9a5ec7959280e171f18
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-11 20:13:37 +0500

    work complete

--- src/components/widgetsm/WidgetSm.jsx
+++ src/components/widgetsm/WidgetSm.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import './WidgetSm.css'
 import { Visibility } from '@material-ui/icons';
-function WidgetSm() {
+function WidgetSm({data, uname, utitle}) {
   return (
     <div className='WidgetSm'>
-    <span className='widgetSmallTitle'>New Join Memeber</span>
+    <span className='widgetSmTitle'>New Join Memeber</span>
     <ul className='WidgetSmList'>
-      <li className='WidgetSmListItem'>
-       <img src='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'  alt='user' className='widgetSmImg'/>
-      <div className='widgetSmUser'>
-<span className='widgetUserName'>Muhammad Fahad Siddiqui</span>
-<span className='widgetUserJob'>Software</span>
-      </div>
-      <button className='widgetSmButton'>
-<Visibility/>
-      </button>
-      </li>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
+      <ListItem name="Fahad" title="SE" image='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'/>
     </ul>
 
+    </div>
+  )
+}
+
+
+export function ListItem({name, title,image}) {
+  return (
+    <div>
+<li className='WidgetSmListItem'>
+       <img src={image}  alt='user' className='widgetSmImg'/>
+      <div className='widgetSmUser'>
+<span className='widgetUserName'>{name}</span>
+<span className='widgetUserJob'>{title}</span>
+      </div>
+      <button className='widgetSmButton'>
+<Visibility className='widgetSmIcon'/>
+Display
+      </button>
+      </li>
     </div>
   )
 }
