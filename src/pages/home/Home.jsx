7dd295b3cd30fd2175e316a52e2c57cccf12f80f
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-11 20:13:20 +0500

    work complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+
 import './Home.css'
 import { userAndSalesData } from '../../Data'
 import WidgetSm from '../../components/widgetsm/WidgetSm'
