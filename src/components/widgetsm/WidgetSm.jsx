27800a5ebf95ef51618d9897897b3021e4a13383
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-11 19:24:44 +0500

    work complete

--- src/components/widgetsm/WidgetSm.jsx
+++ src/components/widgetsm/WidgetSm.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-import './WidgetL.css'
+import './WidgetSm.css'
+import { Visibility } from '@material-ui/icons';
 function WidgetSm() {
   return (
-    <div>WidgetSm</div>
+    <div className='WidgetSm'>
+    <span className='widgetSmallTitle'>New Join Memeber</span>
+    <ul className='WidgetSmList'>
+      <li className='WidgetSmListItem'>
+       <img src='https://avatars.githubusercontent.com/u/61376412?s=400&u=bd4075255e4b63efdd5dc216a0375013fe7e0781&v=4'  alt='user' className='widgetSmImg'/>
+      <div className='widgetSmUser'>
+<span className='widgetUserName'>Muhammad Fahad Siddiqui</span>
+<span className='widgetUserJob'>Software</span>
+      </div>
+      <button className='widgetSmButton'>
+<Visibility/>
+      </button>
+      </li>
+    </ul>
+
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,17 @@ import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './Home.css'
 import { userAndSalesData } from '../../Data'
+import WidgetSm from '../../components/widgetsm/WidgetSm'
+import WidgetLg from '../../components/widgetlg/WidgetLg'
 function Home() {
   return (
     <div className='home'>
     <FeaturedInfo/>
     <Chart title="Sales and Users Analytics" data={ userAndSalesData } userDataKey="users" salesDataKey='sales' grid/>
-    <div className='homeWidgets'></div>
+    <div className='homeWidgets'>
+      <WidgetSm/>
+      <WidgetLg/>
+    </div>
     </div>
   )
 }
