2401db87eed62fe24ffda0765c9a57579c92da54
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-13 19:16:39 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Sidebar from './components/sidebar/Sidebar';
-import Topbar from './components/topbar/Topbar';
-import './App.css'
-import Home from './pages/home/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import UserList from './pages/userList/UserList';
+
+import "./App.css";
+import Home from "./pages/home/Home";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  useRouteMatch,
-  useParams
+  Routes,
 } from "react-router-dom";
 function App() {
   return (
     <Router className="App">
-     <Topbar/>
-     <div className='container'>
-     <Sidebar/>
-     <Home/>
-     </div>
+    
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/users" element={<UserList/>} />
+      </Routes>
+      </div>
     </Router>
   );
 }
