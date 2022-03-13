cd1f79196a323d335c76ed8f3aefb57d722a9820
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-13 20:36:12 +0500

    work complete

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -4,35 +4,35 @@ import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'username', headerName: 'Username', width: 150 },
+  { field: 'email', headerName: 'E-mail',type:'email' , width: 160 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    field: 'status',
+    headerName: 'Status',
+    // type: 'number',
+    width: 100,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
+    field: 'transaction',
+    headerName: 'Transaction',
+    // description: 'This column has a value getter and is not sortable.',
+    // sortable: false,
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    // valueGetter: (params) =>
+    //   `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, username: 'Jon Snow',avatar:"", email: 'Jon@gmail.com', status: 'Active', transaction: 'Rs.1,000' },
+  { id: 2, username: 'Ceriee Lannister',avatar:'', email: 'Cersei@gmail.com', status: 'Active', transaction: 'Rs.2,000' },
+  { id: 3, username: 'Jaime Lannister',avatar:'', email: 'Jaime@gmail.com', status: 'Active' , transaction: 'Rs.3,000' },
+  { id: 4, username: 'Arya Stark',avatar:'', email: 'Arya@gmail.com', status: 'Active' , transaction: 'Rs.4,000' },
+  { id: 5, username: 'Daenerys Targaryen',avatar:'', email: 'Daenerys@gmail.com', status: 'Active' , transaction: 'Rs.5,000' },
+  { id: 6, username: 'Melisandre',avatar:'', email: 'Melisandre@gmail.com', status: 'Active' , transaction: 'Rs.6,000' },
+  { id: 7, username: 'Ferrara Clifford',avatar:'', email: 'Ferrara@gmail.com', status: 'Active' , transaction: 'Rs.7,000' },
+  { id: 8, username: 'Rossini Frances',avatar:'', email: 'Rossini@gmail.com', status: 'Active', transaction: 'Rs.8,000' },
+  { id: 9, username: 'Harvey Roxie',avatar:'', email: 'Harvey@gmail.com', status: 'Active', transaction: 'Rs.9,000' },
 ];
 
 function UserList() {
