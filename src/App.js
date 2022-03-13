915d72728609e611637a244c85f9ead3302e465f
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-13 18:59:52 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import './App.css'
 import Home from './pages/home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
