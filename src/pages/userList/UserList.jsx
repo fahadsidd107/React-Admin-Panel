1f3a756de32c12fafe828913e8fd3b7bd35e4ddc
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-14 16:26:11 +0500

    work complete

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -33,6 +33,7 @@ const rows = [
   { id: 7, username: 'Ferrara Clifford',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Ferrara@gmail.com', status: 'Active' , transaction: 'Rs.7,000' },
   { id: 8, username: 'Rossini Frances',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Rossini@gmail.com', status: 'Active', transaction: 'Rs.8,000' },
   { id: 9, username: 'Harvey Roxie',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Harvey@gmail.com', status: 'Active', transaction: 'Rs.9,000' },
+  { id: 10, username: 'Harvey Roxie',avatar:'https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341', email: 'Harvey@gmail.com', status: 'Active', transaction: 'Rs.9,000' },
 ];
 
 function UserList() {
