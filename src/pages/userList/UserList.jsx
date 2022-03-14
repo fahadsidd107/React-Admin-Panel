eb15df6c0c14333d967f3ca9ce980f791babc3b3
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-14 16:08:21 +0500

    work complete

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -25,8 +25,8 @@ const columns = [
 
 const rows = [
   { id: 1, username: 'Jon Snow',avatar:"https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341", email: 'Jon@gmail.com', status: 'Active', transaction: 'Rs.1,000' },
-  { id: 2, username: 'Ceriee Lannister',avatar:'', email: 'Cersei@gmail.com', status: 'Active', transaction: 'Rs.2,000' },
-  { id: 3, username: 'Jaime Lannister',avatar:'', email: 'Jaime@gmail.com', status: 'Active' , transaction: 'Rs.3,000' },
+  { id: 2, username: 'Ceriee Lannister',avatar:'https://pps.whatsapp.net/v/t61.24694-24/247542397_715408772819277_4530395215229205681_n.jpg?ccb=11-4&oh=282f95e74d80d71950f31e7f60a98887&oe=6235105C', email: 'Cersei@gmail.com', status: 'Active', transaction: 'Rs.2,000' },
+  { id: 3, username: 'Jaime Lannister',avatar:'https://pps.whatsapp.net/v/t61.24694-24/156274807_640738423686807_2563472796988379092_n.jpg?ccb=11-4&oh=ef7c146470c7461fe5754b4bd08f3ed7&oe=62358B4E', email: 'Jaime@gmail.com', status: 'Active' , transaction: 'Rs.3,000' },
   { id: 4, username: 'Arya Stark',avatar:'https://pps.whatsapp.net/v/t61.24694-24/248375694_462098252293667_7625572374605281795_n.jpg?ccb=11-4&oh=eb063fab96b927d89704844e5a5eeb74&oe=6235DA3A', email: 'Arya@gmail.com', status: 'Active' , transaction: 'Rs.4,000' },
   { id: 5, username: 'Daenerys Targaryen',avatar:'', email: 'Daenerys@gmail.com', status: 'Active' , transaction: 'Rs.5,000' },
   { id: 6, username: 'Melisandre',avatar:'', email: 'Melisandre@gmail.com', status: 'Active' , transaction: 'Rs.6,000' },
