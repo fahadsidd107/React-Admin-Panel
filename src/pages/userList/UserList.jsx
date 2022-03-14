cdf74696daafe68217d8c430886ffcb561dc43ea
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-14 16:05:48 +0500

    work complete

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -24,10 +24,10 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, username: 'Jon Snow',avatar:"", email: 'Jon@gmail.com', status: 'Active', transaction: 'Rs.1,000' },
+  { id: 1, username: 'Jon Snow',avatar:"https://pps.whatsapp.net/v/t61.24694-24/234743491_372249084383775_7612602405731643070_n.jpg?ccb=11-4&oh=f06c30ab5033f0dc3b612a1260fa8efd&oe=62353341", email: 'Jon@gmail.com', status: 'Active', transaction: 'Rs.1,000' },
   { id: 2, username: 'Ceriee Lannister',avatar:'', email: 'Cersei@gmail.com', status: 'Active', transaction: 'Rs.2,000' },
   { id: 3, username: 'Jaime Lannister',avatar:'', email: 'Jaime@gmail.com', status: 'Active' , transaction: 'Rs.3,000' },
-  { id: 4, username: 'Arya Stark',avatar:'', email: 'Arya@gmail.com', status: 'Active' , transaction: 'Rs.4,000' },
+  { id: 4, username: 'Arya Stark',avatar:'https://pps.whatsapp.net/v/t61.24694-24/248375694_462098252293667_7625572374605281795_n.jpg?ccb=11-4&oh=eb063fab96b927d89704844e5a5eeb74&oe=6235DA3A', email: 'Arya@gmail.com', status: 'Active' , transaction: 'Rs.4,000' },
   { id: 5, username: 'Daenerys Targaryen',avatar:'', email: 'Daenerys@gmail.com', status: 'Active' , transaction: 'Rs.5,000' },
   { id: 6, username: 'Melisandre',avatar:'', email: 'Melisandre@gmail.com', status: 'Active' , transaction: 'Rs.6,000' },
   { id: 7, username: 'Ferrara Clifford',avatar:'', email: 'Ferrara@gmail.com', status: 'Active' , transaction: 'Rs.7,000' },
