568a2e51c1c82bb1da77a27abb0451a395e6ab2a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-18 03:50:47 +0500

    work complete

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 import ReactDOM from "react-dom";
-import './User.css'
-import { } from '@material-ui/icons';
+import "./User.css";
+import {} from "@material-ui/icons";
 function User() {
   return (
-    <div className='user'>
-    <div className='userTitleContainer'>
-<h1 className='userTitle'>Edit User</h1>
-<button className='userAddButton'>Create</button>
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <button className="userAddButton">Create</button>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">
+          
+        </div>
+        <div className="userUpdate"></div>
+      </div>
     </div>
-  <div className='userContainer'>
-    <div className='userShow'></div>
-    <div className='userUpdate'></div>
-  </div>
-    </div>
-  )
+  );
 }
 
-export default User
+export default User;
