8f3469eb535af892d1b36e961f0b61872824413a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-22 19:37:23 +0500

    work complete

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./User.css";
+import { Link } from "react-router-dom";
 import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from "@material-ui/icons";
 function User() {
   return (
     <div className="user">
       <div className="userTitleContainer">
         <h1 className="userTitle">Edit User</h1>
+        <Link to='/newUser'>
         <button className="userAddButton">Create</button>
+        </Link>
       </div>
       <div className="userContainer">
         <div className="userShow">
