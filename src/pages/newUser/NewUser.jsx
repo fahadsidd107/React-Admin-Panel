a86265ee74294ede54ceaee26b72a3e62cadcf80
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-23 03:52:08 +0500

    work complete

--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -6,12 +6,22 @@ function NewUser() {
     <div className='newUser'>
         <h1 className='newUserTitle'>New User</h1>
         <form className='newUserForm'> 
-<Form label='User Name' placeholder='Fahad'/>
-<Form label='Full Name' placeholder='Fahad Siddiqui'/>
-<Form label='username' placeholder='Fahad'/>
-<Form label='username' placeholder='Fahad'/>
-<Form label='username' placeholder='Fahad'/>
-<Form label='username' placeholder='Fahad'/>
+     
+<Form type='text' label='User Name' placeholder='Fahad'/>
+<Form type='text' label='Full Name' placeholder='Fahad Siddiqui'/>
+<Form type='password' label='Password' placeholder='password'/>
+<Form type='text' label='Phone' placeholder='+92 323 3456123'/>
+<Form type='text' label='Address' placeholder='A Area Karachi'/>
+<label>Gender</label>
+<div className='newUserGender'>
+<input type='radio' name='gender' id='male' value='male'/>
+<label for='male'>Male</label>
+<input type='radio' name='gender' id='female' value='female'/>
+<label for='female'>Female</label>
+<input type='radio' name='gender' id='other' value='other'/>
+<label for='other'>Other</label>
+</div>
+
         </form>
     </div>
   )
@@ -19,12 +29,12 @@ function NewUser() {
 
 
 
-export function Form({label , placeholder }) {
+export function Form({label , placeholder ,type }) {
   return (
     <div className='newUserItem'>
       <label>{label}</label>
-      <input type='text' placeholder={placeholder}/>
-    </div>
+      <input type={type} placeholder={placeholder}/>
+      </div>
   )
 }
 
