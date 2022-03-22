59ffb241935434d3c4163eac76ebf004aa2edccb
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-23 03:38:49 +0500

    work complete

--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -6,13 +6,28 @@ function NewUser() {
     <div className='newUser'>
         <h1 className='newUserTitle'>New User</h1>
         <form className='newUserForm'> 
-<div className='newUserItem'>
-  <label>Username</label>
-  <input type='text' placeholder='Fahad'/>
-</div>
+<Form label='User Name' placeholder='Fahad'/>
+<Form label='Full Name' placeholder='Fahad Siddiqui'/>
+<Form label='username' placeholder='Fahad'/>
+<Form label='username' placeholder='Fahad'/>
+<Form label='username' placeholder='Fahad'/>
+<Form label='username' placeholder='Fahad'/>
         </form>
     </div>
   )
 }
+
+
+
+export function Form({label , placeholder }) {
+  return (
+    <div className='newUserItem'>
+      <label>{label}</label>
+      <input type='text' placeholder={placeholder}/>
+    </div>
+  )
+}
+
+
 
 export default NewUser
