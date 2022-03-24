e396e9103b0e7bb3eb5c19604e04e6737d960987
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-25 00:36:35 +0500

    work complete

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { DataGrid } from "@mui/x-data-grid";
 import { Edit, DeleteOutline } from "@material-ui/icons";
 import { productRows } from "../../Data";
 import { Link } from "react-router-dom";
 import './ProductList.css'
 function ProductList() {
+  const [data, setData] = useState(productRows);
   return (
     <div className='productList'>
             <DataGrid
