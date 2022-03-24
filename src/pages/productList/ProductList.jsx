0438c0b24819705c591d05b08545d1dd607c6e6d
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-25 00:27:12 +0500

    work complete

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { DataGrid } from "@mui/x-data-grid";
+import { Edit, DeleteOutline } from "@material-ui/icons";
+import { userRows } from "../../Data.js";
+import { Link } from "react-router-dom";
 import './ProductList.css'
 function ProductList() {
   return (
     <div className='productList'>
-    ProductList
+            <DataGrid
+          rows={data}
+          disableSelectionOnClick
+          columns={columns}
+          pageSize={8}
+          rowsPerPageOptions={[8]}
+          checkboxSelection
+        />
     </div>
   )
 }
