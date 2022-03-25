70ed2d108633712957162fd11a941ea81ed36cca
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-26 00:12:47 +0500

    work complete

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -10,7 +10,63 @@ function ProductList() {
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
-  
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    {
+      field: "productname",
+      headerName: "Productname",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="productListUser">
+            <img className="productListImg" src={params.row.img} alt="" />
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    { field: "stock", headerName: "Stock", type: "text", width: 160 },
+    {
+      field: "status",
+      headerName: "Status",
+      // type: 'number',
+      width: 160,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      // description: 'This column has a value getter and is not sortable.',
+      // sortable: false,
+      width: 160,
+      // valueGetter: (params) =>
+      //   `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 160,
+      renderCell: (params) => {
+        return (
+          <div>
+            <Link to={"/productEdit/" + params.row.id}>
+              <Edit className="productListEdit" />
+            </Link>
+            <DeleteOutline
+              className="productListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+
+
+
+
+
   return (
     <div className='productList'>
             <DataGrid
