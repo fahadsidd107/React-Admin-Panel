3a61bea42443bd9e46959ff0afa476b351e53354
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-29 10:01:16 +0500

    work complete

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -9,7 +9,27 @@ import { Link } from "react-router-dom";
 function Product() {
   return (
     <div className='product'>
-      
+       <div className="productTitleContainer">
+        <h1 className="productTitle">Product</h1>
+        <Link to="/newproduct">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="productTop">
+          <div className="productTopLeft">
+              <Chart data={productData} dataKey="Sales" title="Sales Performance"/>
+          </div>
+          <div className="productTopRight">
+              <div className="productInfoTop">
+                  <img src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="productInfoImg" />
+                  <span className="productName">Apple Airpods</span>
+              </div>
+              <div className="productInfoBottom">
+                  <div className="productInfoItem">
+                      <span className="productInfoKey">id:</span>
+                      <span className="productInfoValue">123</span>
+                  </div>
+                  
     </div>
   )
 }
