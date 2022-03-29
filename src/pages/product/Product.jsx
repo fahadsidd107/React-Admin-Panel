12852330d1ad213b57a7c1c9c78776d62c919574
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-29 09:58:28 +0500

    work complete

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Product.css'
+import Chart from "../../components/chart/Chart"
+import {productData} from "../../Data"
+import { Publish } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+
 function Product() {
   return (
-    <div className='product'>Product</div>
+    <div className='product'>
+      
+    </div>
   )
 }
 
